e858fa4aed32b9c367643c4344b0f8dd6f883f99
parikhasharma <sharmaparikha@gmail.com>
2023-11-20 15:58:27 +0530

    inital commit

--- src/Components/Navbar/index.js
+++ src/Components/Navbar/index.js
@@ -6,7 +6,7 @@ import './Navbar.css'
 function Navbar() {
   return (
     <div className='navbar'>
-        <img src='logo.png' width={67} height={34}></img>
+        <img src='logo.png' width={67} height={34} alt='Nothing found'></img>
         <SearchBox/>
         <Button>Give Feedback</Button>
     </div>
